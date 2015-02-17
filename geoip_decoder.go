adfdd9c9979ecfca71d6edb27d7305f9615e2199
Martin Loginov <martin.loginov@ut.ee>
2015-02-17 16:24:47 +0200

    Added and option to skip DNS lookup, as it can be quite a costly operation. This is useful if it is known that the source_host_field can only contain IP addresses

--- geoip_decoder.go
+++ geoip_decoder.go
@@ -30,12 +30,18 @@ type GeoIpDecoderConfig struct {
         DatabaseFile  string `toml:"db_file"`
         SourceHostField string `toml:"source_host_field"`
         TargetField   string `toml:"target_field_prefix"`
+
         //When true, all the location info is put into a separate
         //JSON object. The "target_field_prefix" is the name of the field
         //that will contain the raw bytes of the object.
         //When false (the default), all the info is put into separate
         //fields with the prefix "target_field_prefix"
         JSONObject   bool `toml:"raw_json_object"`
+
+        //If true, it will do a DNS lookup on the string contained
+        //in "source_host_field" otherwise it will consider the contents
+        //of "source_host_field" to be a IP address (default)
+        DNSLookup    bool    `toml:"dns_lookup"`
 }
 
 type GeoIpDecoder struct {
@@ -43,6 +49,7 @@ type GeoIpDecoder struct {
         SourceHostField string
         TargetField   string
         JSONObject bool
+        DNSLookup bool
         db            *geoip2.Reader
         pConfig       *PipelineConfig
 }
@@ -76,6 +83,7 @@ func (ld *GeoIpDecoder) Init(config interface{}) (err error) {
         ld.TargetField = conf.TargetField
         ld.SourceHostField = conf.SourceHostField
         ld.JSONObject = conf.JSONObject
+        ld.DNSLookup = conf.DNSLookup
 
         if ld.db == nil {
                 ld.db, err = geoip2.Open(conf.DatabaseFile)
@@ -172,6 +180,7 @@ func (ld *GeoIpDecoder) CreateMessageFields(record *geoip2.City, pack *PipelineP
 
 func (ld *GeoIpDecoder) Decode(pack *PipelinePack) (packs []*PipelinePack, fail error) {
         var hostAddr, _ = pack.Message.GetFieldValue(ld.SourceHostField)
+        var ip net.IP
 
         host, ok := hostAddr.(string)
 
@@ -180,18 +189,28 @@ func (ld *GeoIpDecoder) Decode(pack *PipelinePack) (packs []*PipelinePack, fail 
                 packs = []*PipelinePack{pack}
                 return
         }
-
-        ips, err := net.LookupIP(host)
-        if err != nil {
-                // Could not get an IP for the host, can happen.
+        
+        if ld.DNSLookup {
+            ips, err := net.LookupIP(host)
+            if err != nil {
+                    // Could not get an IP for the host, can happen.
+                    packs = []*PipelinePack{pack}
+                    return
+            }       
+            ip = ips[0]
+        } else {
+            ip = net.ParseIP(host)
+            if ip == nil {
+                //Not a valid IP address in the host string, still we don't
+                //want to send an error
                 packs = []*PipelinePack{pack}
                 return
-        }       
-
+            }
+        }
         if ld.db != nil {
                 //We only check the first IP returned from the local resolver
                 //TODO implement a configuration option to allow checking all IPs
-                rec, err := ld.db.City(ips[0])
+                rec, err := ld.db.City(ip)
                 if err != nil || 
                 (rec.Location.Longitude == 0.0 && rec.Location.Latitude == 0.0){
                         // IP address did not return a valid GeoIp record but that's ok sometimes(private ip?). Return without error.
